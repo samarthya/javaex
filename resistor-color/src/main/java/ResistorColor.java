15876470fb14ffaa880b9b5a38ecc125ba24cf52
Saurabh Sharma <saurabh777@gmail.com>
2020-11-05 19:41:50 +0530

    Version 2 for Resistor Color

--- resistor-color/src/main/java/ResistorColor.java
+++ resistor-color/src/main/java/ResistorColor.java
@@ -1,34 +1,14 @@
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class ResistorColor {
-
-    private final static Map<String, Integer> colorCodes = new HashMap<>() {
-        {
-            put("black", 0);
-            put("brown", 1);
-            put("red", 2);
-            put("orange", 3);
-            put("yellow", 4);
-            put("green", 5);
-            put("blue", 6);
-            put("violet", 7);
-            put("grey", 8);
-            put("white", 9);
-        }
-    };
+    private final static String[] colorList = new String[] { "black", "brown", "red", "orange", "yellow", "green",
+            "blue", "violet", "grey", "white" };
 
     int colorCode(String color) {
-        return colorCodes.containsKey(color) == true ? colorCodes.get(color).intValue() : 0;
+        return Arrays.asList(colorList).indexOf(color);
     }
 
     String[] colors() {
-        String [] strA = new String[10];
-
-        for (String key: colorCodes.keySet()) {
-            strA[colorCodes.get(key)] = key;
-        }
-        return strA;
+        return colorList;
     }
 }
