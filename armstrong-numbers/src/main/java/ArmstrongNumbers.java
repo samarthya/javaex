77d7b283c618faf0ac4f696c0cbe79c99ffe9824
Saurabh Sharma <saurabh777@gmail.com>
2020-11-06 10:13:20 +0530

    Day 1 Complete

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,22 +1,9 @@
 class ArmstrongNumbers {
-    /**
-     * 1 2 3
-     * | | | - 3
-     * | | - 20 +
-     * |- 100 +
-     * @param numberToCheck
-     * @return
-     */
-    boolean isArmstrongNumber(int numberToCheck) {
-//        if (numberToCheck < 10) {
-//            return true;
-//        }
 
+    boolean isArmstrongNumber(int numberToCheck) {
         String str = Integer.toString(numberToCheck);
         int power = str.length();
-
         double d = str.chars().map(Character::getNumericValue).mapToDouble( x -> Math.pow(x, power )).sum();
-        System.out.println( " NUmber: " + d + " == " + numberToCheck);
         return d == numberToCheck;
     }
 
