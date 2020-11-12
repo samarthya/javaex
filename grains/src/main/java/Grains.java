f0183e07409ee918cf849bab6cb4211e29edf67d
Saurabh Sharma <saurabh777@gmail.com>
2020-11-12 15:34:42 +0530

    Using Stream

--- grains/src/main/java/Grains.java
+++ grains/src/main/java/Grains.java
@@ -3,7 +3,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * Grains on a board.
@@ -11,35 +10,15 @@ import java.util.stream.Stream;
  */
 class Grains {
 
-    private Map<Integer, BigInteger> lHM = new LinkedHashMap<>();
-
-    public Grains() {
-        this.Initialize();
-    }
-
-    private void Initialize() {
-        for(int i=0; i<64; i++) {
-            if (i == 0) {
-                lHM.put(Integer.valueOf(1), BigInteger.ONE);
-                continue;
-            }
-            lHM.put(i+1, lHM.get(i).multiply(BigInteger.TWO));
-        }
-    }
-
     BigInteger grainsOnSquare(final int square) {
-        if (square < 1 || square > 64)
-        {
+        if (square < 1 || square > 64) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
-        if (lHM.containsKey(square)) {
-            return lHM.get(square);
-        }
-        return lHM.get(square);
+       return BigInteger.TWO.pow(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        return lHM.values().stream().reduce(BigInteger::add).get();
+        return IntStream.range(1, 65).mapToObj( i -> grainsOnSquare(i)).reduce(BigInteger::add).get();
     }
 
 }
